14e10a5f72da5d63c4b989e78c29a59f8c961baa
Scott Humphries <git@sscotth.io>
2016-03-08 11:58:52 -0600

    initial dictionary class

---
+++ dictionary.js
@@ -0,0 +1,27 @@
+'use strict'
+
+class Dictionary {
+  constructor () {
+    const this.data = []
+  }
+
+  add (index, value) {
+    this.data[index] = value
+  }
+
+  find (index) {
+    return this.data[index]
+  }
+
+  remove (index) {
+    delete this.data[index]
+  }
+
+  showAll () {
+    Object
+      .keys(this.data)
+      .sort()
+      .forEach(key => console.log(key, this.data[key]))
+  }
+}
+
